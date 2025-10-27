3946fdd0b08826a02fdb43611c73377c69c727b1
AnonimHakerrr <oleksandrsoltis@gmail.com>
2025-10-27 22:55:32 +0200

    modal

--- PopcornRadar/Configuration/Modal/MovieDetlail.swift
+++ PopcornRadar/Configuration/Modal/MovieDetlail.swift
@@ -10,12 +10,10 @@ struct MovieDetail: Decodable {
     let releaseDate: String?
     let runtime: Int?           // хвилини
     let genres: [Genre]?
-
     var posterURL: URL? {
         guard let path = posterPath else { return nil }
         return URL(string: "https://image.tmdb.org/t/p/w500\(path)")
     }
-
     var backdropURL: URL? {
         guard let path = backdropPath else { return nil }
         return URL(string: "https://image.tmdb.org/t/p/w780\(path)")
