d98597789e1b3d367d59459500bb1386f3e1e0fe
AnonimHakerrr <oleksandrsoltis@gmail.com>
2025-10-28 20:11:55 +0200

    create movie sevice for key API queries

---
+++ PopcornRadar/Configuration/Networking/MovieService.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+final class MovieService
+{
+    static let sharet = MovieService()
+    private init(){}
+    
+    func getPopularMovies(page: Int = 1)async throws -> MovieResponse{
+        let endpoint = MovieEndpoint.popular(page: page)
+        return try await Networking.shared.request(
+            endpoint: endpoint.path,
+            queryItems: endpoint.queryItems
+        )
+    }
+    
+    func getTrending(timeWindow: String = "day") async throws -> MovieResponse {
+        let endpoint = MovieEndpoint.trending(timeWindow: timeWindow)
+        return try await Networking.shared.request(
+            endpoint: endpoint.path,
+            queryItems: endpoint.queryItems
+        )
+    }
+    
+    func getDetails(for id: Int) async throws -> MovieDetail {
+        let endpoint = MovieEndpoint.details(id: id)
+        return try await Networking.shared.request(
+            endpoint: endpoint.path,
+            queryItems: endpoint.queryItems
+        )
+    }
+    
+    func searchMovies(query: String, page: Int = 1) async throws -> MovieResponse {
+        let endpoint = MovieEndpoint.search(query: query, page: page)
+        return try await Networking.shared.request(
+            endpoint: endpoint.path,
+            queryItems: endpoint.queryItems
+        )
+    }
+}
