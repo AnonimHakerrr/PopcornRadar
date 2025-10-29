c02aef144bcac8102493977fe1812668e31ffc5c
AnonimHakerrr <oleksandrsoltis@gmail.com>
2025-10-29 21:08:53 +0200

    update new main window

--- PopcornRadar/App/PopcornRadarApp.swift
+++ PopcornRadar/App/PopcornRadarApp.swift
@@ -1,17 +1,10 @@
-//
-//  PopcornRadarApp.swift
-//  PopcornRadar
-//
-//  Created by MacBook on 25.10.2025.
-//
-
 import SwiftUI
 
 @main
 struct PopcornRadarApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView() // головна View
         }
     }
 }
