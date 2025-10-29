300e75db546de7243ab5ef9aaf670fbfa35e0335
AnonimHakerrr <oleksandrsoltis@gmail.com>
2025-10-29 21:06:31 +0200

    add new path

--- PopcornRadar/Configuration/Networking/MovieEndpoint.swift
+++ PopcornRadar/Configuration/Networking/MovieEndpoint.swift
@@ -1,22 +1,28 @@
 import Foundation
-
+enum TimeWindow: String { case day, week }
 enum MovieEndpoint
 {
     case popular(page: Int = 1)
-    case trending(timeWindow: String = "day") // "day" або "week"
+    case trending(timeWindow: TimeWindow = .day) // "day" або "week"
     case details(id: Int)
     case search(query: String, page: Int = 1)
+    case discovery(genreID: Int, page: Int = 1)
+    case genres
     
     var path: String {
             switch self {
             case .popular:
                 return "/movie/popular"
-            case .trending:
-                return "/trending/movie"
+            case .trending(let timeWindow):
+                return "/trending/movie/\(timeWindow.rawValue)"
             case .details(let id):
                 return "/movie/\(id)"
             case .search:
                 return "/search/movie"
+            case .discovery:
+                return "/discover/movie"
+            case .genres:
+                return "/genre/movie/list"
             }
         }
 
@@ -24,15 +30,23 @@ enum MovieEndpoint
         switch self {
         case .popular(let page):
             return [URLQueryItem(name: "page", value: "\(page)")]
-        case .trending(let timeWindow):
-            return [URLQueryItem(name: "time_window", value: timeWindow)]
+        case .trending:
+            return []
         case .details:
             return []
+        case .discovery(genreID: let genreID, page: let page):
+            return [
+                URLQueryItem(name: "with_genres", value: "\(genreID)"),
+                URLQueryItem(name: "page", value: "\(page)")
+            ]
         case .search(let query, let page):
             return [
                 URLQueryItem(name: "query", value: query),
                 URLQueryItem(name: "page", value: "\(page)")
             ]
+        case .genres:
+                return [URLQueryItem(name: "language", value: "uk-UA")]
+           
         }
     }
 }
